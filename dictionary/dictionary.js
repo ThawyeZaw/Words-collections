ab3194042858c4b44d8286a32285590e4d20e8b8
Thaw ye Zaw <thawyezaw@gmail.com>
2023-11-06 22:30:31 +0630

    commit
    Filter feature added,
    show and hide functions added

--- dictionary/dictionary.js
+++ dictionary/dictionary.js
@@ -2,24 +2,29 @@ const words_ = document.querySelector('#words'),
   showPlace = document.querySelector('p')
 let collection = [], values = {}
 
+/* --------- fetch the collection json -------- */
 fetch('/./assets/words.json')
   .then(res => res.json())
   .then(words => {
     values = words
+    /* ------- work thorough the collection ------- */
     for (const word in words) {
       collection.push(word)
       words_.innerHTML += `<option>${word}</option>`
     }
     console.log(collection)
   })
+  /* ------ check error and alert if found ------ */
   .catch(err => {
     alert("An error has occured")
     console.log(err)
   })
 
+/* ------- function for finding the word ------ */
 function findWord() {
+  /* --------------- get the word --------------- */
   let word = document.querySelector("#word").value
-  console.log(word)
+  /* ---- check if the word is in collection ---- */
   if (collection.includes(word)) {
     const { meaning, category, synonyms } = values[word]
     let categories = [], synonym = []
@@ -27,6 +32,7 @@ function findWord() {
     category.map(c => categories.push(`<li>${c}</li>`))
     let output_1 = categories.join(""),
       output_2 = synonym.join("")
+    /* ---------- show the result in html --------- */
     showPlace.innerHTML = `
             <p style="display: flex;">
               <b>${word}</b>
@@ -46,5 +52,8 @@ function findWord() {
 								</ul>
 							</li>
 						</ul>`
-  } else { console.log("no") }
+      
+  }
+  /* --------- if the word is not found --------- */
+  else { console.log("no") }
 }
--- words_collection/collection.js
+++ words_collection/collection.js
@@ -1,19 +1,9 @@
 const list = document.querySelector(".list")
+let fetchedData
 
-fetch("/./assets/words.json")
-  .then(res => res.json())
-  .then(json => {
-    for (const word in json) {
-      console.log(word, json[word].category)
-      let { meaning, category, synonyms } = json[word],
-        categories = [], synonym = []
-      synonyms.map(s => synonym.push(`<li>${s}</li>`))
-      category.map(c => categories.push(`<li>${c}</li>`))
-      let output_1 = categories.join(""),
-        output_2 = synonym.join("")
-
-      list.innerHTML += `
-      <li class="word">
+const displayWords = (word, meaning, category, synonyms) => {
+  list.innerHTML += `
+      <li class="word" id ="${word}">
       <p style="display: flex;">
         <b>${word}</b>
         <span>--- ${meaning}</span>
@@ -22,24 +12,163 @@ fetch("/./assets/words.json")
 						<li>
 							<i>categories</i>
 							<ul>
-              ${output_1}
+              ${category}
 							</ul>
 						</li>
 						<li>
 							<i>synonyms</i>
 							<ul>
-              ${output_2}
+              ${synonyms}
 							</ul>
 						</li>
 					</ul>
           <hr>
 				</li>`
+}
+
+/* --------- fetch the collection json -------- */
+fetch("/./assets/words.json")
+  .then(res => res.json())
+  .then(json => {
+    fetchedData = json
+    for (const word in json) {
+      /* ------------- declare variables ------------ */
+      let { meaning, category, synonyms } = json[word],
+        categories = [], synonym = []
+      /* ----- display the catgory and synonyms ----- */
+      synonyms.map(s => synonym.push(`<li>${s}</li>`))
+      category.map(c => categories.push(`<li>${c}</li>`))
+      let output_1 = categories.join(""),
+        output_2 = synonym.join("")
+      /* ------------ show result in html ----------- */
+      displayWords(word, meaning, output_1, output_2)
     }
   })
+  /* ------ check error and alert if found ------ */
   .catch(err => {
     alert("An error has occured!")
     console.log(err)
   })
 
+/* --------- filter by chosen category -------- */
+function filterWords() {
+  let selectedWords = []
+  for (const word in fetchedData) {
+    let chosenCategory = document.querySelector("input[name='category']:checked")
+    if (fetchedData[word].category.includes(chosenCategory.value)) {
+      let text = [word, fetchedData[word]]
+      selectedWords.push(text)
+    }
+  }
+  list.innerHTML = ""
+  selectedWords.forEach(word => {
+    console.log(word)
+    let synonyms = word[1].synonyms, category = word[1].category,
+      synonym = [], categories = []
+    synonyms.map(s => synonym.push(`<li>${s}</li>`))
+    category.map(c => categories.push(`<li>${c}</li>`))
+    let output_1 = categories.join(""),
+      output_2 = synonym.join("")
+    displayWords(word[0], word[1].meaning, output_1, output_2)
+    toggleDefinition()
+  })
+}
+/* ------- removing the filter function ------- */
+function removeFilter() {
+  list.innerHTML = ''
+  for (const word in fetchedData) {
+    /* ------------- declare variables ------------ */
+    let { meaning, category, synonyms } = fetchedData[word],
+      categories = [], synonym = []
+    /* ----- display the catgory and synonyms ----- */
+    synonyms.map(s => synonym.push(`<li>${s}</li>`))
+    category.map(c => categories.push(`<li>${c}</li>`))
+    let output_1 = categories.join(""),
+      output_2 = synonym.join("")
+    /* ------------ show result in html ----------- */
+    displayWords(word, meaning, output_1, output_2)
+    toggleDefinition()
+  }
+}
 
-// console.log(object)
+/* ~~~~~~~~~ display codes start here ~~~~~~~~~ */
+
+/* ----- show the definitions of all words ---- */
+const show = () => {
+  const words = document.querySelectorAll('.word')
+  words.forEach(word => word.classList.add('shown'))
+
+  document.querySelectorAll('span').forEach(a => {
+    a.style.display = 'flex'
+  })
+  document.querySelectorAll('.about').forEach(a => {
+    a.style.display = 'flex'
+  })
+  document.querySelectorAll('hr').forEach(a => {
+    a.style.display = 'flex'
+  })
+},
+/* ----- hide the definitions of all words ---- */
+  hide = () => {
+    const words = document.querySelectorAll('.word')
+    words.forEach(word => word.classList.remove('shown'))
+
+    document.querySelectorAll('span').forEach(a => {
+      a.style.display = 'none'
+    })
+    document.querySelectorAll('.about').forEach(a => {
+      a.style.display = 'none'
+    })
+    document.querySelectorAll('hr').forEach(a => {
+      a.style.display = 'none'
+    })
+  }
+/* ---------- show the filter buttons --------- */
+document.querySelector('#showfilter').addEventListener('click', e => {
+  document.querySelector('#filter').style.display = "block"
+})
+/* ---------- hide the filter buttons --------- */
+document.querySelector('#hidefilter').addEventListener('click', e => {
+  document.querySelector('#filter').style.display = "none"
+})
+
+/* - function to toggle definiton of one word - */
+function toggleDefinition () {
+  const words = document.querySelectorAll('.word')
+
+  words.forEach(word => {
+    word.addEventListener('click', e => {
+      /* --------- show defintion if hidden --------- */
+      if (!(word.classList.contains('shown'))) {
+        word.classList.add('shown')
+        document.querySelectorAll(`#${word.id} span`).forEach(a => {
+          a.style.display = 'flex'
+        })
+        document.querySelectorAll(`#${word.id} .about`).forEach(a => {
+          a.style.display = 'flex'
+        })
+        document.querySelectorAll(`#${word.id} hr`).forEach(a => {
+          a.style.display = 'flex'
+        })
+        /* ---------- hide defintion if shown --------- */
+      }else if (word.classList.contains('shown')) {
+        word.classList.remove('shown')
+        document.querySelectorAll(`#${word.id} span`).forEach(a => {
+          a.style.display = 'none'
+        })
+        document.querySelectorAll(`#${word.id} .about`).forEach(a => {
+          a.style.display = 'none'
+        })
+        document.querySelectorAll(`#${word.id} hr`).forEach(a => {
+          a.style.display = 'none'
+        })
+      } else {
+        alert('error')
+      }
+    })
+  })
+}
+/* ------ add toggle function after load ------ */
+setTimeout(() => {
+  toggleDefinition()
+}, 1000);
